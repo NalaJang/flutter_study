649bbd7dd5527c68228647f3a23be42da447329a
nala0897@naver.com <nala0897@naver.com>
2023-07-15 21:30:01 +1200

    Add:GestureDetector 위젯 추가

--- lib/main.dart
+++ lib/main.dart
@@ -52,97 +52,106 @@ class _LogInState extends State<LogIn> {
       ),
       body: Builder(
         builder:(context) {
-          // arrow 버튼을 누르면 push 로 화면 이동을 할 때
-          // 필요한 context 를 위해 SingleChildScrollView 를 Builder 로 감싸주었다.
-          return SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(padding: EdgeInsets.only(top:50)),
-                const Center(
-                  child: Image(
-                    image: AssetImage('image/chef.gif'),
-                    width: 170.0,
-                    height: 190.0,
+
+          // 텍스트 입력 도중 화면 빈 공간을 클릭했을 때 키보드가 내리기 위해 GestureDetector 추가
+          return GestureDetector(
+
+            onTap: (){
+              FocusScope.of(context).unfocus();
+            },
+
+            // arrow 버튼을 누르면 push 로 화면 이동을 할 때
+            // 필요한 context 를 위해 SingleChildScrollView 를 Builder 로 감싸주었다.
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(padding: EdgeInsets.only(top:50)),
+                  const Center(
+                    child: Image(
+                      image: AssetImage('image/chef.gif'),
+                      width: 170.0,
+                      height: 190.0,
+                    ),
                   ),
-                ),
-                Form(
-                    child: Theme(
-                        data: ThemeData(
-                            primaryColor: Colors.teal,
-                            inputDecorationTheme: const InputDecorationTheme(
-                                labelStyle: TextStyle(
-                                    color: Colors.teal,
-                                    fontSize: 15.0
-                                )
-                            )
-                        ),
-                        child: Container(
-                          padding: EdgeInsets.all(40.0),
-                          child: Column(
-                            children: [
-                              TextField(
-                                controller: id_controller,
-                                decoration: InputDecoration(
-                                    labelText: 'Enter "dice"'
-                                ),
-                                keyboardType: TextInputType.emailAddress,
-                              ),
-
-                              TextField(
-                                controller: pw_controller,
-                                decoration: InputDecoration(
-                                    labelText: 'Enter Password'
-                                ),
-                                keyboardType: TextInputType.text,
-                                obscureText: true,
-                              ),
-
-                              const SizedBox(height: 40.0,),
-
-                              ButtonTheme(
-                                minWidth: 100.0,
-                                height: 50.0,
-                                child: ElevatedButton(
-                                    onPressed: (){
-
-                                      if( id_controller.text == 'dice'
-                                          && pw_controller.text == '1234' ) {
-
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (BuildContext context) => Dice()
-                                        ));
-
-                                      } else if( id_controller.text == 'dice'
-                                          && pw_controller.text != '1234' ) {
-
-                                        showSnackBarPwError(context);
-
-                                      } else if( id_controller.text != 'dice'
-                                          && pw_controller.text == '1234' ) {
-
-                                        showSnackBarIdError(context);
-
-                                      } else {
-
-                                        showSnackBarLoginError(context);
-                                      }
-
-                                    },
-                                    child: const Icon(
-                                      Icons.arrow_forward,
-                                      color: Colors.white,
-                                      size: 35.0,
-                                    )
-                                ),
-                              ),
-                            ],
+                  Form(
+                      child: Theme(
+                          data: ThemeData(
+                              primaryColor: Colors.teal,
+                              inputDecorationTheme: const InputDecorationTheme(
+                                  labelStyle: TextStyle(
+                                      color: Colors.teal,
+                                      fontSize: 15.0
+                                  )
+                              )
                           ),
-                        )
-                    )
-                )
-              ],
+                          child: Container(
+                            padding: EdgeInsets.all(40.0),
+                            child: Column(
+                              children: [
+                                TextField(
+                                  controller: id_controller,
+                                  decoration: InputDecoration(
+                                      labelText: 'Enter "dice"'
+                                  ),
+                                  keyboardType: TextInputType.emailAddress,
+                                ),
+
+                                TextField(
+                                  controller: pw_controller,
+                                  decoration: InputDecoration(
+                                      labelText: 'Enter Password'
+                                  ),
+                                  keyboardType: TextInputType.text,
+                                  obscureText: true,
+                                ),
+
+                                const SizedBox(height: 40.0,),
+
+                                ButtonTheme(
+                                  minWidth: 100.0,
+                                  height: 50.0,
+                                  child: ElevatedButton(
+                                      onPressed: (){
+
+                                        if( id_controller.text == 'dice'
+                                            && pw_controller.text == '1234' ) {
+
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (BuildContext context) => Dice()
+                                          ));
+
+                                        } else if( id_controller.text == 'dice'
+                                            && pw_controller.text != '1234' ) {
+
+                                          showSnackBarPwError(context);
+
+                                        } else if( id_controller.text != 'dice'
+                                            && pw_controller.text == '1234' ) {
+
+                                          showSnackBarIdError(context);
+
+                                        } else {
+
+                                          showSnackBarLoginError(context);
+                                        }
+
+                                      },
+                                      child: const Icon(
+                                        Icons.arrow_forward,
+                                        color: Colors.white,
+                                        size: 35.0,
+                                      )
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                      )
+                  )
+                ],
+              ),
             ),
           );
         },
