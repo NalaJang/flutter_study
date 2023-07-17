ae65badc5b96c64ff7c39f486134aa6d896d70e1
nala0897@naver.com <nala0897@naver.com>
2023-07-17 18:42:56 +1200

    Fix: 버튼을 클릭할 때마다 주사위 값을 변경하기 위해서 Stateless -> StatefulWidget 으로 변경

--- lib/dice.dart
+++ lib/dice.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Dice());
 
-
-class Dice extends StatelessWidget {
+// 버튼을 클릭할 때마다 주사위 값을 변경하기 위해서 Stateless -> StatefulWidget 으로 변경
+class Dice extends StatefulWidget {
   const Dice({Key? key}) : super(key: key);
 
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
