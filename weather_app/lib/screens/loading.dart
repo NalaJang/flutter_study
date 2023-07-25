3d7ab19f27e6bdee8cf9f694c33265089a55fa48
nala0897@naver.com <nala0897@naver.com>
2023-07-25 17:11:15 +1200

    Add:dummy data 를 통해 날씨 정보 가져오기

--- weather_app/lib/screens/loading.dart
+++ weather_app/lib/screens/loading.dart
@@ -17,6 +17,8 @@ class _LoadingState extends State<Loading> {
     super.initState();
     // 버튼 클릭 없이 앱이 실행될 때 바로 사용자의 위치를 찾기 위함.
     getLocation();
+
+    fetchData();
   }
 
   void getLocation() async {
@@ -31,6 +33,16 @@ class _LoadingState extends State<Loading> {
     } catch(e) {
       print('There was a problem with the Internet connection.');
     }
+  }
+
+  // dummy data 를 통해 날씨 정보 가져오기
+  void fetchData() async {
+    var uri = Uri.parse('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1');
+    // get() 은 Future 메소드이기 때문에 async 방식으로 작동해야 한다.
+    // 따라서 weather data 를 모두 가져올 때까지 기다려야 함을 뜻한다.
+    // 그래서 await 키워드를 넣어주고 와 fetchData()를 async 방식으로 해주어야 한다.
+    Response response = await get(uri);
+    print('response: $response');
   }
 
   @override
