28644f15bcdc7a247e9bea5a1ce15d7a47a5770e
nala0897@naver.com <nala0897@naver.com>
2023-07-27 16:42:03 +1200

    Fix:날짜 정보와 날씨 정보 column 공간 띄우기

--- weather_app/lib/screens/weather_screen.dart
+++ weather_app/lib/screens/weather_screen.dart
@@ -78,73 +78,80 @@ class _WeatherScreenStateState extends State<WeatherScreenState> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: 150.0,
-                          ),
-                          Text(
-                            'Seoul',
-                            style: GoogleFonts.lato(
-                              fontSize: 35.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white
+
+                  // 날짜 정보와 날씨 정보의 column 을 띄우기 위해 spaceBetween 를 사용했지만
+                  // 날씨 정보의 column 은 이미 충분한 공간을 사용하고 있기 때문에 공간이 떨어지지 않는다.
+                  // 그래서 Expanded widget 으로 공간을 억지로 떨어뜨리게 한다.
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 150.0,
                             ),
-                          ),
-                          Row(
-                            children: [
-                              TimerBuilder.periodic(
-                                  (Duration(minutes: 1)),
-
-                                  // builder widget 은 반드시 widget 을 return 해야 한다.
-                                  builder: (context){
-                                    print(getSystemTime());
-                                    return Text(
-                                      '${getSystemTime()}',
-                                      style: GoogleFonts.lato(
-                                        fontSize: 16.0,
-                                        color: Colors.white
-                                      ),
-                                    );
-                                  },
-                              ),
-                              Text(
-                                DateFormat(' - EEEE, ').format(date),
-                                style: GoogleFonts.lato(
-                                    fontSize: 16.0,
-                                    color: Colors.white
-                                ),
-                              ),
-                              Text(
-                                DateFormat('d MMM, yyy').format(date),
-                                style: GoogleFonts.lato(
-                                    fontSize: 16.0,
-                                    color: Colors.white
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-
-                      // 날씨 정보를 위한 column
-                      Column(
-                        children: [
-                          Text(
-                            'Seoul',
-                            style: GoogleFonts.lato(
+                            Text(
+                              'Seoul',
+                              style: GoogleFonts.lato(
                                 fontSize: 35.0,
                                 fontWeight: FontWeight.bold,
                                 color: Colors.white
+                              ),
                             ),
-                          ),
-                        ],
-                      )
-                    ],
+                            Row(
+                              children: [
+                                TimerBuilder.periodic(
+                                    (Duration(minutes: 1)),
+
+                                    // builder widget 은 반드시 widget 을 return 해야 한다.
+                                    builder: (context){
+                                      print(getSystemTime());
+                                      return Text(
+                                        '${getSystemTime()}',
+                                        style: GoogleFonts.lato(
+                                          fontSize: 16.0,
+                                          color: Colors.white
+                                        ),
+                                      );
+                                    },
+                                ),
+                                Text(
+                                  DateFormat(' - EEEE, ').format(date),
+                                  style: GoogleFonts.lato(
+                                      fontSize: 16.0,
+                                      color: Colors.white
+                                  ),
+                                ),
+                                Text(
+                                  DateFormat('d MMM, yyy').format(date),
+                                  style: GoogleFonts.lato(
+                                      fontSize: 16.0,
+                                      color: Colors.white
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+
+                        // 날씨 정보를 위한 column
+                        Column(
+                          children: [
+                            Text(
+                              'Seoul',
+                              style: GoogleFonts.lato(
+                                  fontSize: 35.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
 
                   // 대기 지수를 보여주기 위한 column
