e70ce4e00f3ce367e91259b4119bf88ee6b1347c
nala0897@naver.com <nala0897@naver.com>
2023-07-28 15:56:55 +1200

    Add: main screen ui 디자인

--- yummy_chat_lecture/lib/screens/main_screen.dart
+++ yummy_chat_lecture/lib/screens/main_screen.dart
@@ -29,9 +29,125 @@ class _LoginSignupScreenState extends State<LoginSignupScreen> {
                 image: DecorationImage(
                   image: AssetImage('image/red.jpg'),
                   fit: BoxFit.fill
-                )
+                ),
+              ),
+
+              child: Container(
+                padding: EdgeInsets.only(top: 90, left: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: 'Welcome',
+                        style: TextStyle(
+                          letterSpacing: 1.0,
+                          fontSize: 25,
+                          color: Colors.white
+                        ),
+
+                        children: [
+                          TextSpan(
+                            text: ' to Yummy chat!',
+                            style: TextStyle(
+                              letterSpacing: 1.0,
+                              fontSize: 25,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+
+                    Text(
+                      'Signup to continue',
+                      style: TextStyle(
+                          letterSpacing: 1.0,
+                          color: Colors.white
+                      ),
+                    )
+                  ],
+                ),
               ),
             )
+          ),
+
+          Positioned(
+            top: 180,
+            child:
+            Container(
+              padding: EdgeInsets.all(20.0),
+              height: 280.0,
+              width: MediaQuery.of(context).size.width -40,
+              margin: EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    blurRadius: 15,
+                    spreadRadius: 5
+                  )
+                ],
+              ),
+
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+
+                      // login
+                      Column(
+                        children: [
+                          Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Palette.textColor1
+                            ),
+                          ),
+
+                          Container(
+                            margin: EdgeInsets.only(top: 3),
+                            height: 2,
+                            width: 55,
+                            color: Colors.orange,
+                          )
+                        ],
+                      ),
+
+                      // signup
+                      Column(
+                        children: [
+                          Text(
+                            'SIGNUP',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Palette.textColor1
+                            ),
+                          ),
+
+                          Container(
+                            margin: EdgeInsets.only(top: 3),
+                            height: 2,
+                            width: 55,
+                            color: Colors.orange,
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ),
         ],
       ),
