6904a8bad3073f79ccbf799443f52e66486e4e58
nala0897@naver.com <nala0897@naver.com>
2023-07-31 14:58:00 +1200

    Add: flutter core engine 초기화

--- yummy_chat_lecture/lib/main.dart
+++ yummy_chat_lecture/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:yummy_chat_lecture/screens/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  // Firebase.initializedApp()과 같은 비동기 메소드를 사용하기 위해
+  // flutter core engine 초기화가 필요하다.
+  // 따라서 메인 메소드 내에서 비동기 방식으로 ensureInitialized 메소드를 먼저 불러와야 한다.
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
