200c00dc69d9d8ca3230e439af37932a449bce47
nala0897@naver.com <nala0897@naver.com>
2023-08-03 17:16:32 +1200

    Add: 로그인, 로그아웃에 따라 사용자 관리를 해주기 위해 streamBuilder 위젯 추가

--- yummy_chat_lecture/lib/main.dart
+++ yummy_chat_lecture/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:yummy_chat_lecture/screens/main_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:yummy_chat_lecture/screens/chat_screen.dart';
+import 'package:yummy_chat_lecture/screens/main_screen.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   // Firebase.initializedApp()과 같은 비동기 메소드를 사용하기 위해
@@ -28,7 +30,18 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue
       ),
 
-      home: LoginSignupScreen(),
+      home: StreamBuilder(
+        // 이 stream 은 authentication state 가 바뀔 때마다 새로운 value 을 전달해준다.
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // snapshot 이 인증 받은 token(권한) 을 가지고 있다면(참고로 token 은 firebase_auth 에서 관리한다.)
+          // 즉, 유효한 사용자라면,
+          if( snapshot.hasData ) {
+            return ChatScreen();
+          }
+          return LoginSignupScreen();
+        },
+      ),
     );
   }
 }
