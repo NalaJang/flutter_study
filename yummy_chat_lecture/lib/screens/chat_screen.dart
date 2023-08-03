136c4e4224ae50afe9bec19c5bae575bb621dc50
nala0897@naver.com <nala0897@naver.com>
2023-08-03 16:31:38 +1200

    Add: Firebase 에서 데이터 가져오는 동안은 로딩화면 보이기

--- yummy_chat_lecture/lib/screens/chat_screen.dart
+++ yummy_chat_lecture/lib/screens/chat_screen.dart
@@ -55,9 +55,17 @@ class _ChatScreenState extends State<ChatScreen> {
       ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance
-            .collection('chats/nJhOHExkhIMjzuL7MHbP/message').snapshots(),
+            .collection('chats/fdHhcNf5ovXtvfBwcoyV/message')
+            .snapshots(),
         builder: (BuildContext context,
             AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+
+          // 데이터를 가져오는 동안은 null error 가 뜨기 때문에 그 사이에는 화면에 로딩 이미지를 띄워준다.
+          if( snapshot.connectionState == ConnectionState.waiting ) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
 
           final docs = snapshot.data!.docs;
           return ListView.builder(
