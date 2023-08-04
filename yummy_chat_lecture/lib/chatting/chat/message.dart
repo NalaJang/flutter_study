ab5c8854789c03f296c6464605b1f0519ac04600
nala0897@naver.com <nala0897@naver.com>
2023-08-04 22:54:05 +1200

    Add: 채팅 화면에 메시지 보여주기

---
+++ yummy_chat_lecture/lib/chatting/chat/message.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages extends StatelessWidget {
+  const Messages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 채팅 메시지를 구독하고 있다가 보여줘야 하므로 StreamBuilder widget 사용
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      // AsyncSnapshot class 는 최신의 snapshot 을 가져오기 위해 꼭 필요한 요소이다.
+      builder: (context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+        if( snapshot.connectionState == ConnectionState.waiting ) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        final chatDocs = snapshot.data!.docs;
+        // 데이터가 전달되었을 때,
+        return ListView.builder(
+          itemCount: chatDocs.length,
+          itemBuilder: (context, index) {
+            // 화면에 메시지 보여주기(메시지 리스트 렌더링)
+            return Text(chatDocs[index]['text']);
+          },
+        );
+      },
+    );
+  }
+}
