08aeb70f2d4250893b0df87ff863fa11b8a5fe00
nala0897@naver.com <nala0897@naver.com>
2023-08-12 18:56:23 +1200

    Add:ChangeNotifier 를 mixin

--- provider_ex/lib/fish_model.dart
+++ provider_ex/lib/fish_model.dart
@@ -1,7 +1,14 @@
-class FishModel {
+import 'package:flutter/cupertino.dart';
+
+class FishModel with ChangeNotifier{
   final String name;
-  final int number;
+  int number;
   final String size;
 
   FishModel({required this.name, required this.number, required this.size});
+
+  void changeFishNumber() {
+    number++;
+    notifyListeners();
+  }
 }
