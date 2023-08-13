8c72910724c7bf8ae23ddcb539c9f9c67f88a519
nala0897@naver.com <nala0897@naver.com>
2023-08-13 19:42:21 +1200

    Add:login page UI

---
+++ firestore_ex/lib/login.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final hello = 'Hello';
+  final welcomeBack = 'Welcome back';
+  final signIn = 'Sign in';
+  final notAMember = 'Not a member?';
+  final registerNow = ' Register Now!';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.phone_android,
+              size: 100,
+            ),
+
+            SizedBox(height: 30),
+
+            Text(hello),
+
+            SizedBox(height: 10),
+
+            Text(
+              welcomeBack,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+
+            SizedBox(height: 50),
+
+            // email
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12)
+                ),
+
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 20.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none, hintText: 'Email'
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10,),
+
+            // password
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12)
+                ),
+
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 20.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: 'Password'
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10,),
+
+            // 로그인 버튼
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(12)
+                ),
+
+                child: Center(
+                  child: Text(
+                    signIn,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 25,),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(notAMember),
+                Text(
+                  registerNow,
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- firestore_ex/lib/main.dart
+++ firestore_ex/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'login.dart';
+
 void main() {
   runApp(const MyApp());
 }
