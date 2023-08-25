807c3d41b0a18e4ff77ed04e3d4b815690019196
nala0897@naver.com <nala0897@naver.com>
2023-08-25 19:36:22 +1200

    Add:SliverAppBar

--- scrolling_effect/lib/scroll_effect.dart
+++ scrolling_effect/lib/scroll_effect.dart
@@ -5,11 +5,21 @@ class ScrollEffect extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: CustomScrollView(
         // slivers 란 하나의 스크롤링을 의미하는 것이기 때문에
         // 이 slivers 알규먼트를 통해서 여러 개의 스크롤링을 나열, 구현할 수 있다.
         slivers: [
+          SliverAppBar(
+            title: Text('Sliver appBar',),
+            backgroundColor: Colors.blueGrey,
+            expandedHeight: 200,
+            leading: Icon(Icons.arrow_back),
+            actions: [
+              Icon(Icons.settings),
+              SizedBox(width: 12,)
+            ],
+          )
         ],
       ),
     );
