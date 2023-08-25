f3d9a844d2af0d3be9e1cd2b04a894c4c2158911
nala0897@naver.com <nala0897@naver.com>
2023-08-25 15:35:11 +1200

    Add:login.php 접근 할 변수 추가

--- mysql_git/lib/api/api.dart
+++ mysql_git/lib/api/api.dart
@@ -3,5 +3,6 @@ class API {
   // API 에 접근
   static const hostConnectUser = "$hostConnect/user";
   static const signup = "$hostConnect/user/signup.php";
+  static const login = "$hostConnect/user/login.php";
   static const validateEmail = "$hostConnect/user/validate_email.php";
 }
