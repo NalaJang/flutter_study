dac7d2ddc4a8366ebb9f57e16893730209b0bb76
nala0897@naver.com <nala0897@naver.com>
2024-02-19 11:07:55 +0900

    기본적인 Localization 구현

--- localization_ex/lib/main.dart
+++ localization_ex/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'myHomePage.dart';
 
@@ -13,6 +15,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Localization ex',
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
       home: MyHomePage(),
     );
   }
