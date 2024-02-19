d481963082200a43ce1b5c8b8718e79a250e0ec9
nala0897@naver.com <nala0897@naver.com>
2024-02-19 15:32:36 +0900

    이전 코드 제거

--- localization_ex/lib/MyHomePage.dart
+++ localization_ex/lib/MyHomePage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -10,14 +9,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          // AppLocalizations.of(context)!.helloWorld
           Intl.message('test')
         ),
       ),
 
       body: Center(
         child: Text(
-          // AppLocalizations.of(context)!.hello('코린이')
           Intl.message('test2')
         ),
       ),
