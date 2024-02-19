da938c32a6388e361ef1d6c507f2465f8289fd67
nala0897@naver.com <nala0897@naver.com>
2024-02-19 13:46:21 +0900

    Localization 을 통해 text 보이기

--- localization_ex/lib/MyHomePage.dart
+++ localization_ex/lib/MyHomePage.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          AppLocalizations.of(context)!.helloWorld
+        ),
+      ),
+
+      body: Center(
+        child: Text(
+          AppLocalizations.of(context)!.hello('코린이')
+        ),
+      ),
+    );
   }
 }
