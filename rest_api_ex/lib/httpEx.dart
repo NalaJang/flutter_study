0265eb110e9b320a99c26bf1fda306e15d66f5df
nala0897@naver.com <nala0897@naver.com>
2024-02-26 08:53:31 +0900

    rest_api_ex

---
+++ rest_api_ex/lib/httpEx.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HttpEx extends StatelessWidget {
+  const HttpEx({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Http ex'),
+      ),
+    );
+  }
+}
