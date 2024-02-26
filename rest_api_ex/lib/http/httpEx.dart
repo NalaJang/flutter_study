44e6b576f130e975ccdb34aaa31bac00a3c8a596
nala0897@naver.com <nala0897@naver.com>
2024-02-26 16:41:41 +0900

    feat: 불러온 이미지, 데이터 보여주기

--- rest_api_ex/lib/http/httpEx.dart
+++ rest_api_ex/lib/http/httpEx.dart
@@ -24,10 +24,55 @@ class HttpEx extends StatelessWidget {
               child: ListView.builder(
                 itemCount: state.modelList.length,
                 itemBuilder: (context, index) {
-                  return Column(
-                    children: [
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            // 보여줄 이미지
+                            SizedBox(
+                              width: state.modelList[index].width * 0.02,
+                              height: state.modelList[index].height * 0.02,
 
-                    ],
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(12),
+                                child: Image.network(
+                                  state.modelList[index].downloadUrl,
+                                  fit: BoxFit.cover,
+                                  // 이미지 로딩 시 보여줄 화면
+                                  loadingBuilder: (context, Widget child, ImageChunkEvent? loadingProcess) {
+                                    if( loadingProcess == null ) {
+                                      return child;
+                                    }
+                                    return Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+                                        color: const Color.fromRGBO(205, 197, 197, 1)
+                                      ),
+                                      child: const Center(
+                                        child: CircularProgressIndicator(
+                                          color: Colors.amber,
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ),
+
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("ID : ${state.modelList[index].id}"),
+                                Text("Author : ${state.modelList[index].author}"),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   );
                 }
               ),
