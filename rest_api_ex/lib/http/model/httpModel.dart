a572eec5df7ac2f8b001f23048dfcf1864de9f15
nala0897@naver.com <nala0897@naver.com>
2024-02-26 09:17:00 +0900

    feat: model 클래스 추가

---
+++ rest_api_ex/lib/http/model/httpModel.dart
@@ -0,0 +1,17 @@
+class HttpModel {
+  final String id;
+  final String author;
+  final int width;
+  final int height;
+  final String url;
+  final String downloadUrl;
+
+  HttpModel({
+    required this.id,
+    required this.author,
+    required this.width,
+    required this.height,
+    required this.url,
+    required this.downloadUrl,
+  });
+}
--- rest_api_ex/lib/main.dart
+++ rest_api_ex/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'httpEx.dart';
+import 'http/httpEx.dart';
 
 void main() {
   runApp(const MyApp());
