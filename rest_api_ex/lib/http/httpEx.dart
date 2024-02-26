bfc85345e1b64d532e1034373de1141d9435bf32
nala0897@naver.com <nala0897@naver.com>
2024-02-26 10:38:01 +0900

    feat: 데이터를 보여줄 UI 구축

--- rest_api_ex/lib/http/httpEx.dart
+++ rest_api_ex/lib/http/httpEx.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rest_api_ex/http/provider/httpProvider.dart';
 
 class HttpEx extends StatelessWidget {
   const HttpEx({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Http ex'),
+    return ChangeNotifierProvider<HttpProvider> (
+
+      create: (_) => HttpProvider()..getPhotos(),
+
+      child: Consumer<HttpProvider>(
+        builder: (context, state, child) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('Http ex'),),
+
+            body: ListView.builder(
+              itemCount: state.modelList.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  child: Text(state.modelList[index].id),
+                );
+              }
+            ),
+          );
+        },
       ),
     );
   }
