cdbb6900d032d7db9680fd03e4cf3ab7c27d69c7
nala0897@naver.com <nala0897@naver.com>
2024-02-27 19:02:52 +0900

    feat: view page 생성

---
+++ rest_api_ex/lib/dio2/ui/dio_result_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DioResultPage extends StatelessWidget {
+  const DioResultPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dio, Retrofit Ex'),
+      ),
+
+      body: Center(
+
+      ),
+    );
+  }
+}
