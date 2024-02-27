33cdbef512dbed11a769269e564384fdcb10798f
nala0897@naver.com <nala0897@naver.com>
2024-02-27 10:53:43 +0900

    add: colorListResponse.dart 변수 추가

--- rest_api_ex/lib/dio2/model/colorListResponse.dart
+++ rest_api_ex/lib/dio2/model/colorListResponse.dart
@@ -5,9 +5,21 @@ part 'colorListResponse.g.dart';
 @JsonSerializable()
 class ColorListResponse {
 
+  int page;
+  @JsonKey(name: 'per_page')
+  int perPage;
+  int total;
+  @JsonKey(name: 'total_pages')
+  int totalPages;
   ColorModel data;
 
-  ColorListResponse({required this.data});
+  ColorListResponse({
+    required this.page,
+    required this.perPage,
+    required this.total,
+    required this.totalPages,
+    required this.data,
+  });
 
   factory ColorListResponse.fromJson(Map<String, dynamic> json) => _$ColorListResponseFromJson(json);
   Map<String, dynamic> toJson() => _$ColorListResponseToJson(this);
